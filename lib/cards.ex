8088d005dc64ee9f6f2adbc4f154b1ad38c42218
Yusuf Ganiyu <airscholar@gmail.com>
2021-09-12 18:26:51 +0100

    Added card operations

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,52 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
+  @spec create_deck :: list
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamond"]
+    # tempCards =
+    #   for value <- values do
+    #     for suit <- suits do
+    #       "#{value} of #{suit}"
+    #     end
+    #   end
+    # List.flatten(tempCards)
 
-  @doc """
-  Hello world.
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-  ## Examples
+  @spec shuffle(any) :: list
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-      iex> Cards.hello()
-      :world
+  @spec contains(any, any) :: boolean
+  def contains(deck, card) do
+    Enum.member?(deck, card)
+  end
 
-  """
-  def hello do
-    "Hi there!"
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  @spec save(
+          any,
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | char,
+              binary | []
+            )
+        ) :: :ok | {:error, atom}
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(file_name, binary)
+  end
+
+  def load(file_name) do
+    case File.read(file_name) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exists"
+    end
   end
 end
